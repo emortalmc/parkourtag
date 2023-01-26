ce228592f0cc1d28cb7e6f55ec4007a9bacdef64
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-01-26 14:33:53 +0000

    Switched to commonModules

--- src/main/java/dev/emortal/minestom/parkourtag/Entrypoint.java
+++ src/main/java/dev/emortal/minestom/parkourtag/Entrypoint.java
@@ -1,20 +1,13 @@
 package dev.emortal.minestom.parkourtag;
 
 import dev.emortal.minestom.core.MinestomServer;
-import dev.emortal.minestom.core.module.chat.ChatModule;
-import dev.emortal.minestom.core.module.core.CoreModule;
-import dev.emortal.minestom.core.module.kubernetes.KubernetesModule;
-import dev.emortal.minestom.core.module.permissions.PermissionModule;
 import dev.emortal.minestom.gamesdk.GameSdkModule;
 
 public class Entrypoint {
     public static void main(String[] args) {
         new MinestomServer.Builder()
-                .module(KubernetesModule.class, KubernetesModule::new)
+                .commonModules()
                 .module(GameSdkModule.class, GameSdkModule::new)
-                .module(CoreModule.class, CoreModule::new)
-                .module(PermissionModule.class, PermissionModule::new)
-                .module(ChatModule.class, ChatModule::new)
                 .module(ParkourTagModule.class, ParkourTagModule::new)
                 .build();
     }
